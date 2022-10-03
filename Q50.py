e517fefc5536838972e9ff9aabed5b4a7495f55c
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2022-10-03 01:12:56 -0400

    Create Q50.py

---
+++ Q50.py
@@ -0,0 +1,14 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+
+        if n == 1:
+            return x
+
+        if n == -1:
+            return 1 / x
+
+        result = self.myPow(x, n // 2)
+
+        return result * result * (x if n % 2 else 1)
